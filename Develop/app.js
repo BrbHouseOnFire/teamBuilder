46573eb72a7dd7d9bca36b3e14fb873149c3aa14
Brb House on Fire <bobbywoj@sbcglobal.net>
2019-11-25 12:54:15 -0600

    functionality complete

--- Develop/app.js
+++ Develop/app.js
@@ -8,6 +8,21 @@ const outputPath = path.resolve(__dirname, "output", "team.html");
 const render = require("./lib/render");
 const teamMembers = [];
 const idArray = [];
+let html =
+`<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+        <title>Department Roster</title>
+    </head>
+    <body>
+      <div class="jumbotron">
+        <h1 class= "text-center font-weight-bold">Department Members</h1>
+      </div>
+    <div class="row d-flex">`;
 
 
 function appMenu() {
@@ -75,8 +90,6 @@ function appMenu() {
         createTeam();
       });
     }
-
-
     function createTeam() {
       inquirer.prompt([
         {
@@ -105,9 +118,59 @@ function appMenu() {
     function addEngineer() {
       inquirer.prompt([
         /* prompts here */
+        {
+          type: "input",
+          name: "engineerName",
+          message: "What is the engineer's name?",
+          validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          }
+        },
+        {
+          type: "input",
+          name: "engineerId",
+          message: "What is the engineer's id?",
+          validate: answer => {
+            const pass = answer.match(
+              /^[1-9]\d*$/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a positive number greater than zero.";
+          }
+        },
+        {
+          type: "input",
+          name: "engineerEmail",
+          message: "What is the engineer's email?",
+          validate: answer => {
+            const pass = answer.match(
+              /\S+@\S+\.\S+/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a valid email address.";
+          }
+        },
+        {
+          type: "input",
+          name: "engineerGithub",
+          message: "What is the engineer's GitHub username?",
+          validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          }
+        }
       ]).then(answers => {
         /* create engineer */
-        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerOfficeNumber);
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
         teamMembers.push(engineer);
         idArray.push(answers.engineerId);
         createTeam();
@@ -116,20 +179,100 @@ function appMenu() {
     function addIntern() {
       inquirer.prompt([
         /* prompts here */
+        {
+          type: "input",
+          name: "internName",
+          message: "What is the intern's name?",
+          validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter at least one character.";
+          }
+        },
+        {
+          type: "input",
+          name: "internId",
+          message: "What is the intern's id?",
+          validate: answer => {
+            const pass = answer.match(
+              /^[1-9]\d*$/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a positive number greater than zero.";
+          }
+        },
+        {
+          type: "input",
+          name: "internEmail",
+          message: "What is your the intern's email?",
+          validate: answer => {
+            const pass = answer.match(
+              /\S+@\S+\.\S+/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Please enter a valid email address.";
+          }
+        },
+        {
+          type: "input",
+          name: "internSchool",
+          message: "What school does the intern go to?",
+          validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Please enter a positive number greater than zero.";
+          }
+        }
       ]).then(answers => {
         /* create intern */
-        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internOfficeNumber);
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
         teamMembers.push(intern);
         idArray.push(answers.internId);
         createTeam();
       });
     }
 
-    function buildTeam() {
-      const htmlFile = `<html>${teamMembers[0].name}</html>`
-      fs.writeFileSync(outputPath, htmlFile, "utf-8");
-    }
-    createManager();
+    
+  function buildTeam() {
+    let uniqueValue;
+    teamMembers.forEach((e, index) => {
+        if (e.getRole() === "Manager"){
+          uniqueValue = `Office #: ${e.getOfficeNumber()}`
+        }
+        if (e.getRole()=== "Engineer"){
+          uniqueValue = `GitHub Username: ${e.getGithub()}`
+        }
+        if (e.getRole()=== "Intern"){
+          uniqueValue = `School of Attendance: ${e.getSchool()}`
+        }
+          let userDiv = `<div class="col-3 m-5" id = "roster${index}">
+              <div class="card text-white bg-dark">
+                <div class="card-header text-center font-weight-bold">
+                  ${e.getRole()}
+                </div>
+                <div class ="card-body">
+                  <div> Name: ${e.getName()}</div>
+                  <div> ID: ${e.getId()}</div>
+                  <div> Email: ${e.getEmail()}</div>
+                  <div> ${uniqueValue} </div>
+                </div>
+              </div>
+            </div>`;
+          html += userDiv;
+    });
+    html += `</div></body></html>`;
+    console.log("Writing HTML file to /Develop/output/team.html");
+    return fs.writeFileSync("./Develop/output/team.html", html , "utf-8")
+  };
+
+
+  createManager();
 }
 appMenu();
 
